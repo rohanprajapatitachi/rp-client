0b7e19b54597f774c239e956ff82f44d7ee01b1f
sanjayguwaju <sanjayguwaju@gmail.com>
2023-11-09 22:58:53 +0545

    fetch data from the server using async

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const todoList = document.getElementById("todo-list");
+    const listElement = document.querySelector("#todo-list tbody");
 
-    // Fetch todos from the API
-    fetch("http://localhost:3000/todos/get-all-todos")
-        .then((response) => response.json())
-        .then((data) => {
-            // Iterate over the todos and create list items
-            data.forEach((todo) => {
-                const listItem = document.createElement("li");
-                listItem.innerHTML = `
-                    <h2>${todo.title}</h2>
-                    <p><strong>Author:</strong> ${todo.author}</p>
-                    <p>${todo.content}</p>
-                `;
-                todoList.appendChild(listItem);
+    async function fetchTodos() {
+        try {
+            const response = await fetch("http://localhost:3001/todos/get-all-todos");
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const todos = await response.json();
+            console.log("todos ~~~~>", todos);
+            listElement.innerHTML = ""; // Clear existing todos
+            todos.forEach(todo => {
+                const row = `<tr>
+                                <td>${todo?.title}</td>
+                                <td>
+                                    <button onclick="editTodo('${todo?._id}')">Edit</button>
+                                    <button onclick="deleteTodo('${todo?._id}')">Delete</button>
+                                </td>
+                             </tr>`;
+                listElement.innerHTML += row;
             });
-        })
-        .catch((error) => {
-            console.error("Error fetching todos:", error);
-        });
-});
+        } catch (error) {
+            console.error('Error:', error);
+            alert("There was an error fetching the todo list.");
+        }
+    }
+
+    window.deleteTodo = (id) => {
+        localStorage.setItem('editingTodoId', id);
+        if (!confirm("Are you sure you want to delete this todo?")) {
+            return;
+        }
+        fetch(`http://localhost:3001/todos/delete-todo/${id}`, { method: 'DELETE' })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(() => fetchTodos()) // Refresh list after delete
+            .catch(error => {
+                console.error('Error:', error);
+                alert("There was an error deleting the todo.");
+            });
+    };
+
+    window.editTodo = (id) => {
+        // You can either use localStorage or query parameters. Here's using query params for example:
+        localStorage.setItem('editingTodoId', id);
+        window.location.href = `edit.html?id=${id}`;
+    };
+
+    fetchTodos();
